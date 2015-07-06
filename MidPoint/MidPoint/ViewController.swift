e9aaf472c9dc48d56418d912921fd57e92e6dfc0
William Alvelos <wi_alvelos@hotmail.com>
2015-07-06 11:18:19 -0300

    Commit Will

--- MidPoint/MidPoint/ViewController.swift
+++ MidPoint/MidPoint/ViewController.swift
@@ -14,17 +14,27 @@ import CoreLocation
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet var localTextField: UITextField!
+    
     let googleAPIKey: String = "AIzaSyDHIzjnXRJtWRDpPsux99HhTwjOmcLQplU"
     
     var locationManager = CLLocationManager()
+    
     @IBOutlet weak var mapView: MKMapView!
     let radius: CLLocationDistance = 100
 
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        localTextField.resignFirstResponder()
+//        var coor2:CLLocationCoordinate2D = CLLocationCoordinate2DMake(-23.690055, -46.901234)
+//        addPointsOfInterest("", name: localTextField.text, location: coor2);
+    }
     
     override func viewWillAppear(animated: Bool) {
         mapView.delegate = self
         locationManager.delegate = self
         mapView.showsUserLocation = true
+        localTextField.hidden = true
         
         locationManager.requestAlwaysAuthorization()
         
@@ -32,6 +42,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         var coor2:CLLocationCoordinate2D = CLLocationCoordinate2DMake(-23.690055, -46.901234)
         
         getMiddleDistanceFromPoints(coorSP, coordinate2: coor2)
+        addPointsOfInterest("restaurant", name: "", location: coor2);
         //addLocationsFromGoogle()
         
     }
@@ -39,25 +50,41 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     private func addPointsOfInterest(type: String, name: String, location: CLLocationCoordinate2D) {
         
         var url: String
+        var url2: String
         
         if name.isEmpty {
-            url = "https://maps.googleapis.com/maps/api/place/search/json?location=-23.670055,-46.701234&radius=1000&types=" + type + "&sensor=true&key=" + googleAPIKey
+            url = "https://maps.googleapis.com/maps/api/place/search/json?location=\(location.latitude),\(location.longitude)&radius=1000&types=" + type + "&sensor=true&key=" + googleAPIKey
+
+            
+            url2 = "https://api.foursquare.com/v2/venues/search?client_id=AF0RKOHW12ZHKMLCLO0C5LV0CA3CQEFC2RBIV4TDUQARJCE0&client_secret=VBQQDPB5OHA4NFRX5O02KZR5FVDNNBKC1HLB1YKJUTTLODNB&v=20130815&ll=\(location.latitude),\(location.longitude)&query="
         }
         
         else {
-            url = "https://maps.googleapis.com/maps/api/place/search/json?location=-23.670055,-46.701234&radius=1000&types=" + type + "&name=" + name + "&sensor=true&key=" + googleAPIKey
+            url = "https://maps.googleapis.com/maps/api/place/search/json?location=\(location.latitude),\(location.longitude)&radius=100&types=" + type + "&name=" + name + "&sensor=true&key=" + googleAPIKey
+            
+            url2 = "https://api.foursquare.com/v2/venues/search?client_id=AF0RKOHW12ZHKMLCLO0C5LV0CA3CQEFC2RBIV4TDUQARJCE0&client_secret=VBQQDPB5OHA4NFRX5O02KZR5FVDNNBKC1HLB1YKJUTTLODNB&v=20130815&ll=\(location.latitude),\(location.longitude)&query=" + name
+            
         }
 
         var data: NSData = NSData(contentsOfURL: NSURL(string: url)!)!
         
-        var json: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+        var data1: NSData = NSData(contentsOfURL: NSURL(string: url2)!)!
         
-        var places: NSArray = json.objectForKey("results") as! NSArray
+        var jsonGooogle: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+        
+        var jsonFourSquare: AnyObject! = NSJSONSerialization.JSONObjectWithData(data1, options: NSJSONReadingOptions.MutableContainers, error: nil)
+        
+        var places: NSArray = jsonGooogle.objectForKey("results") as! NSArray
+        
+        var venues: NSDictionary = jsonFourSquare.objectForKey("response") as! NSDictionary
+        
+        var placesFourSquare: NSArray = venues.objectForKey("venues") as! NSArray
         
         for(var x = 0; x < places.count; x++) {
             
             var place: NSDictionary = places.objectAtIndex(x) as! NSDictionary
             var geo: NSDictionary = place.objectForKey("geometry") as! NSDictionary
+            var icon: String = place.objectForKey("icon") as! String
             var loc: NSDictionary = geo.objectForKey("location") as! NSDictionary
             var name: String = place.objectForKey("name") as! String
             var lat: NSNumber = loc.objectForKey("lat") as! NSNumber
@@ -72,10 +99,45 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             
             mapView.addAnnotation(point)
             
+            
+        }
+        
+        
+        
+        for(var x = 0; x < placesFourSquare.count; x++) {
+            
+            var placeFourSquare: NSDictionary = placesFourSquare.objectAtIndex(x) as! NSDictionary
+            
+            var locFourSquare: NSDictionary = placeFourSquare.objectForKey("location") as! NSDictionary
+            
+            var nameFourSquare: String = placeFourSquare.objectForKey("name") as! String
+            
+            var latFourSquare: NSNumber = locFourSquare.objectForKey("lat") as! NSNumber
+            
+            var lonFourSquare: NSNumber = locFourSquare.objectForKey("lng") as! NSNumber
+            
+            var pointFourSquare: MKPointAnnotation = MKPointAnnotation()
+            
+            var coordinateFourSquare: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latFourSquare.doubleValue, lonFourSquare.doubleValue)
+            
+            
+            pointFourSquare.title = nameFourSquare
+            pointFourSquare.coordinate = coordinateFourSquare
+            
+            mapView.addAnnotation(pointFourSquare)
+            
         }
         
     }
     
+    @IBAction func actionSearch(sender: AnyObject) {
+        
+        if(localTextField.hidden){
+            localTextField.hidden = false
+        }else{
+            localTextField.hidden = true
+        }
+    }
 
     func locationManager(manager: CLLocationManager!,
         didChangeAuthorizationStatus status: CLAuthorizationStatus) {
@@ -97,18 +159,31 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                 // Start location services
                 locationManager.startUpdatingLocation()
             }
+            
+            
     }
     
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         
-        var coorSP:CLLocationCoordinate2D = CLLocationCoordinate2DMake(-23.670055, -46.701234)
+//        var coorSP:CLLocationCoordinate2D = CLLocationCoordinate2DMake(-23.670055, -46.701234)
         
-        let region = MKCoordinateRegionMakeWithDistance(coorSP, radius, radius)
-        //mapView.setRegion(region, animated: true)
         
+        locationManager.stopUpdatingLocation()
+
+        var locationArray = locations as NSArray
+        var locationObj = locationArray.lastObject as! CLLocation
+        var coord = locationObj.coordinate
+        
+        let region = MKCoordinateRegionMakeWithDistance(coord, radius, radius)
+
+        
+        mapView.setRegion(region, animated: true)
+        
+        addPointsOfInterest("", name: localTextField.text, location: coord);
+
     }
-    
+
     
     func getMiddleDistanceFromPoints(coordinate1: CLLocationCoordinate2D, coordinate2: CLLocationCoordinate2D) {
         
